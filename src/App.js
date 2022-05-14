cef81eceec57b69e1ad96431510582b16c15609b
Thongrob Tipsurat <thongrob.tsr@gmail.com>
2022-05-14 23:42:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+import DocumentTitle from "react-document-title"
 
-function App() {
+const App = ()=> {
+  
+  const [countries, setCountries] = useState([])
+
+  const [word, setWord] = useState("")
+
+  const [dataFilter] = useState(["capital","common"])
+
+
+
+  useEffect(()=>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res=>res.json())
+    .then(data=>{
+      // console.log(data)
+      setCountries(data)
+    })
+  },[])
+
+  //ฟังก์ชันการใส่ comma คั่นตัวเลข
+  const formatNumber=(num)=> {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+   //ฟังก์ชันการค้นหาข้อมูลที่สนใจ
+  const searchCountries = (countries)=>{
+    return countries.filter((item)=>{      
+      return dataFilter.some((filter)=>{        
+        if(item[filter]){
+          return item[filter].toString().toLowerCase().indexOf(word.toLowerCase()) > -1                    
+        }                          
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+      <DocumentTitle title="Search-filter Countries"/>
+      <div className="search">
+        <label htmlFor="search-form">
+          <input type="text" 
+          className="search-input" 
+          placeholder="ค้นหาข้อมูลประเทศที่ต้องการ (เมืองหลวง, ชื่อประเทศ)" 
+          value={word}
+          onChange={(e)=>setWord(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <ul className="row">
+        {searchCountries(countries).map((item, index)=>{
+          return(
+            <li key={index}>
+              <div className="card">
+                <div className="card-title">
+                  <img src={item.flags.png} alt={item.name.common} />
+                </div>
+                <div className="card-body">
+                  <div className="card-description">
+                    <h2>{item.name.common}</h2>
+                    <ol className="card-list">
+                      <li>ประชากร : <span>{formatNumber(item.population)}</span> คน </li>
+                      <li>ภูมิภาค : <span>{item.region}</span></li>
+                      <li>เมืองหลวง : <span>{item.capital}</span></li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
